093c516e203cf060b0d5dc91b05116de3e420638
janmejayasahoo <jjsahoo.09@gmail.com>
2017-09-24 18:04:31 -0500

    Adding Tree Problems

---
+++ Tree/src/MaximumDepthofBinaryTree.java
@@ -0,0 +1,13 @@
+
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+
+        if(root==null)
+            return 0;
+        if(root.left==null && root.right==null)
+            return 1;
+
+        return 1+ Math.max(maxDepth(root.left),maxDepth(root.right));
+
+    }
+}
